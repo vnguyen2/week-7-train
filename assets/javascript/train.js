8be4647b9410cd31ef15a928fc7ebdc4281a4277
Vincent Nguyen <vnguyen2@gmail.com>
2016-09-12 00:15:22 -0500

    firebase db functioning

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -4,24 +4,27 @@ $("#addTrainBtn").on("click", function(){
 	// Grabs user input
 	var trainName = $("#trainNameInput").val().trim();
 	var trainDest = $("#destInput").val().trim();
-	var trainStart = moment($("#startInput").val().trim(), "HHmm").format("X");
+	var trainStart = $("#startInput").val().trim();
 	var trainFreq = $("#freqInput").val().trim();
+
+	console.log($("#startInput").val().trim());
 
 	var newTrain = {
 		name:  trainName,
-		role: trainRole,
+		dest: trainDest,
 		start: trainStart,
-		rate: trainFreq
+		freq: trainFreq
 	}
+
+	console.log(newTrain);
 
 	trainData.push(newTrain);
 
 	console.log(newTrain.name);
-	console.log(newTrain.role); 
+	console.log(newTrain.dest); 
 	console.log(newTrain.start);
-	console.log(newTrain.rate)
+	console.log(newTrain.freq)
 
-	// Alert
 	alert("Train successfully added");
 
 	// Clears all of the text-boxes
@@ -31,5 +34,25 @@ $("#addTrainBtn").on("click", function(){
 	$("#freqInput").val("");
 
 	return false;
+});
+
+trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+	console.log(childSnapshot.val());
+
+	// Store everything into a variable.
+	var trainName = childSnapshot.val().name;
+	var trainDest = childSnapshot.val().dest;
+	var trainStart = childSnapshot.val().start;
+	var trainFreq = childSnapshot.val().freq;
+
+	console.log(trainName);
+	console.log(trainDest);
+	console.log(trainStart);
+	console.log(trainFreq);
+
+
+	$("#trainloyeeTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + "xxxxxxx" + "</td><td>" + "Xxxxxx" + "</td></tr>");
+
 });
 
